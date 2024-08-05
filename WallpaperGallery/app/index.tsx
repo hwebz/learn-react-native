60897ea94c2b26b50d9bfd1214e3dceec084e0ad
Ha Manh Do <hadm.haui@gmail.com>
2024-08-05 17:25:35 +0700

    UI for welcome + home screens

--- WallpaperGallery/app/index.tsx
+++ WallpaperGallery/app/index.tsx
@@ -1,10 +1,99 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Image, Pressable } from 'react-native'
 import React from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { hp, wp } from '@/helpers/common'
+import { LinearGradient } from 'expo-linear-gradient'
+import Animated, { FadeInDown } from 'react-native-reanimated'
+import { theme } from '@/constants/theme'
+import { useRouter } from 'expo-router'
 
 export default function WelcomeScreen() {
+  const router = useRouter()
   return (
-    <View>
-      <Text>WelcomeScreen</Text>
+    <View style={styles.container}>
+      <StatusBar style='light' />
+      <Image
+        source={require('../assets/images/welcome.jpg')}
+        style={styles.bgImage}
+        resizeMode='cover'
+      />
+      {/* Linear Gradient */}
+      <Animated.View
+        style={{
+          flex: 1
+        }}
+        entering={FadeInDown.duration(600)}
+      >
+        <LinearGradient
+          colors={['rgba(255, 255, 255, 0)', 'rgba(255, 255, 255, 0.5)', 'white', 'white']}
+          style={styles.gradient}
+          start={{ x: 0.5, y: 0 }}
+          end={{ x: 0.5, y: 0.8 }}
+        />
+        {/* Content */}
+        <View style={styles.contentContainer}>
+          <Animated.Text entering={FadeInDown.delay(400).springify()} style={styles.title}>
+            Pixels
+          </Animated.Text>
+          <Animated.Text entering={FadeInDown.delay(500).springify()} style={styles.punchline}>
+            Every Pixel Tells a Story
+          </Animated.Text>
+          <Animated.View entering={FadeInDown.delay(600).springify()}>
+            <Pressable style={styles.startButton} onPress={() => router.push('home')}>
+              <Text style={styles.startText}>Start Explore</Text>
+            </Pressable>
+          </Animated.View>
+        </View>
+      </Animated.View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  bgImage: {
+    width: wp(100),
+    height: hp(100),
+    position: 'absolute'
+  },
+  gradient: {
+    width: wp(100),
+    height: hp(65),
+    bottom: 0,
+    position: 'absolute'
+  },
+  contentContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    gap: 14
+  },
+  title: {
+    fontSize: hp(7),
+    color: theme.colors.neutral(0.9),
+    fontWeight: `${theme.fontWeights.bold}` as any
+  },
+  punchline: {
+    fontSize: hp(2),
+    letterSpacing: 1,
+    marginBottom: 10,
+    fontWeight: `${theme.fontWeights.medium}` as any,
+  },
+  startText: {
+    fontSize: hp(3),
+    color: theme.colors.white,
+    fontWeight: `${theme.fontWeights.medium}` as any,
+    letterSpacing: 1
+  },
+  startButton: {
+    marginBottom: 50,
+    backgroundColor: theme.colors.neutral(0.9),
+    padding: 15,
+    paddingHorizontal: 90,
+    borderRadius: theme.radius.xl,
+    borderCurve: 'continuous'
+
+  }
+})
